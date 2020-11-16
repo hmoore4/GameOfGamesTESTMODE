2f1c0fce0cc1c05bf95ec301bcdf241ddd9987f8
annachin <74474510+annachin@users.noreply.github.com>
2020-11-16 16:28:13 -0500

    Update CoinFlip.java

--- CoinFlip.java
+++ CoinFlip.java
@@ -11,31 +11,33 @@ import java.util.*;
 
 public class CoinFlip{
 
-	private int setRounds; //stores the number of rounds the user wants to play
-	private String userGuess; //the user's guess whether if it's heads or tails
-	private int userCount; //the amount wins the user has
-	private int programCount; //the amount of wins the computer has
-	private String computerChoice; //the correct answer of which side the coin lands on
-	private int numofSides = 1; //used to randomly determine which side the coins lands on
+    private int setRounds; //stores the number of rounds the user wants to play
+    private String userGuess; //the user's guess whether if it's heads or tails
+    private int userCount; //the amount wins the user has
+    private int programCount; //the amount of wins the computer has
+    private String computerChoice; //the correct answer of which side the coin lands on
+    private int numofSides = 1; //used to randomly determine which side the coins lands on
 
-	//finding the side the coin lands on
-	Random random = new Random();
-	int computerguess = random.nextInt(numofSides) + 1;
+    //finding the side the coin lands on
+    Random random = new Random();
+    int computerguess = random.nextInt(numofSides) + 1;
 
-	//Scanner for input 
-	static Scanner input = new Scanner(System.in);
+    //Scanner for input 
+    static Scanner input = new Scanner(System.in);
 
-	//establishing the computer's guess
-	if(computerguess == 1){
-		computerchoice = 'heads';
-	}else{
-		computerchoice = 'tails';
-	}
+   //establishing the computer's guess
+   public static void compGuess(computerguess){
+      if(computerguess == 1){
+      	 computerchoice = "heads";
+       }else{
+          computerchoice = "tails";
+       }
+   }
 
-	/**
-	* method that will display the rules of the game
-	* @return: void
-	*/
+   /**
+   * method that will display the rules of the game
+   * @return: void
+   */  
    public static void displayGame(){
 	    System.out.println("\t|---------------------------------------------------------------------------------------------------------|");
 	    System.out.println("\t|                                        Welcome to Coin Flip                                             |");
@@ -58,9 +60,10 @@ public class CoinFlip{
   * @return void
   */
   public static void userguess(){
-  		System.out.print("Enter your Guess: ");
-		userGuess = input.nextLine(); 
-		System.out.println();
+  	System.out.print("Enter your Guess: ");
+	userGuess = input.nextLine(); 
+	
+	System.out.println();
   }
   
   /**
@@ -68,9 +71,9 @@ public class CoinFlip{
   * @return void
   */
   public static void rounds(int rounds){
-  		System.out.print("Enter the amount of rounds you want to play: ");
-  		setRounds = input.nextInt();
-  		Systm.out.println();
+  	System.out.print("Enter the amount of rounds you want to play: ");
+  	setRounds = input.nextInt();
+  	Systm.out.println();
   }
   	
   
@@ -79,29 +82,29 @@ public class CoinFlip{
   * @return void
   */
   public static void coinFlip(String guess, int rounds){
-  		for(int i= 0; i < Math.ceil(rounds/2); i++)
-  			if(guess == computerChoice){
-  				System.out.println("You won this round!");
-  				userCount++; 
+  	for(int i= 0; i < Math.ceil(rounds/2); i++)
+  		if(guess == computerChoice){
+  			System.out.println("You won this round!");
+  			userCount++; 
 
-  			}else{
-  				System.out.println("You lost this round :(");
-  				programCount++;
-  			}
-  		scoreboard(userCount, programCount);
+  		}else{
+  			System.out.println("You lost this round :(");
+  			programCount++;
+  		}
+  	scoreboard(userCount, programCount);
   }
   /**
   * Method that prints the score and which player has won the game.
   * @return void
   */
   public static void scoreboard(int userCount, int programCount){
-  		System.out.println("User's Score: " + userCount); 
-  		System.out.println("Computer's Score: " + programCount);
-  		if(userCount > programCount){
-  			System.out.println("Congrats! You won the game!");
-  		}else{
-  			System.out.println("Bummer! You lost. Try again next time.");
-  		}
+  	System.out.println("User's Score: " + userCount); 
+  	System.out.println("Computer's Score: " + programCount);
+  	if(userCount > programCount){
+  		System.out.println("Congrats! You won the game!");
+  	}else{
+  		System.out.println("Bummer! You lost. Try again next time.");
+  	}
   }
 
   /**
